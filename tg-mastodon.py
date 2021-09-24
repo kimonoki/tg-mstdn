98dd308a6f76a98beaa8db7744f373b34a49df03
kimonoki <mo_noki@yahoo.com>
2021-09-24 11:24:32 +0900

    update #3

--- tg-mastodon.py
+++ tg-mastodon.py
@@ -5,109 +5,116 @@ import logging
 import os
 import sys
 
-TOKEN = 'your telegram bot TOKEN'
+TOKEN = 'telegram bot token'
 
 
 # welcome string
-def start(bot, update):
-    bot.send_message(
-        chat_id=update.message.chat_id,
-        text="Hello! You can use this bot to send toots! Type /help for more information")
+def start(update, context):
+    bot_send_message(
+        update, context, "Hello! You can use this bot to send toots! Type /help for more information")
 
 
-def help(bot, update):
-    bot.send_message(
-        chat_id=update.message.chat_id,
+def help(update, context):
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
         text=help_message, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
 
 
-def my_info(bot, update):
+def my_info(update, context):
     user_id = update.effective_user.id
     if user_id in accounts_information and all(
             k in accounts_information[user_id] for k in ("instances address", "access token")):
-        verify_account(bot, update, user_id)
+        verify_account(update, context, user_id)
     else:
-        notify_incomplete_information(bot, update)
+        notify_incomplete_information(update, context)
 
 
 # check telegram user id
-def send_user_id(bot, update):
+def send_user_id(update, context):
     user_id = update.effective_user.id
-    bot.send_message(chat_id=update.message.chat_id, text="Your telegram id: {}".format(user_id))
+    context.bot.send_message(chat_id=update.message.chat_id,
+                             text="Your telegram id: {}".format(user_id))
 
 
-def set_instance_address(bot, update, args):
-    if args:
+def set_instance_address(update, context):
+
+    if context.args:
         user_id = update.effective_user.id
         if user_id not in accounts_information:
             accounts_information[user_id] = {}
-        accounts_information[user_id]["instances address"] = args[0]
+        accounts_information[user_id]["instances address"] = context.args[0]
         if "access token" in accounts_information[user_id]:
             set_mastodon_client(user_id)
-            verify_account(bot, update, user_id)
+            verify_account(update, context, user_id)
         else:
-            notify_missing_information(bot, update, 'Now please set your access token')
+            bot_send_message(
+                update, context, 'Now please set your access token')
 
 
-def set_access_token(bot, update, args):
-    if args:
+def set_access_token(update, context):
+
+    if context.args:
         user_id = update.effective_user.id
         if user_id not in accounts_information:
             accounts_information[user_id] = {}
-        accounts_information[user_id]["access token"] = args[0]
+        accounts_information[user_id]["access token"] = context.args[0]
         if "instances address" in accounts_information[user_id]:
             set_mastodon_client(user_id)
-            verify_account(bot, update, user_id)
+            verify_account(update, context, user_id)
         else:
-            notify_missing_information(bot, update, 'Now please set your instance url')
+            bot_send_message(
+                update, context, 'Now please set your instance url')
 
 
 # send toot to your instance
-def toot(bot, update, visibility='public'):
+def toot(update, context, visibility='public'):
+
     user_id = update.effective_user.id
     if user_exists(user_id) and user_information_is_complete(user_id):
         mastodon_client = accounts_information[user_id]["mastodon client"]
         if update.message.photo:  # for images, save locally and upload to mastodon
-            file_path = BASE_FILE_PATH.format(update.message.chat_id, update.message.message_id)
+            file_path = BASE_FILE_PATH.format(
+                update.message.chat_id, update.message.message_id)
             if update.message.caption:
-                media_id = upload_media(bot, update, mastodon_client, file_path)
+                media_id = upload_media(
+                    update, mastodon_client, file_path)
                 if not mastodon_client.status_post(update.message.caption, visibility=visibility,
                                                    media_ids=media_id).id:
-                    notify_toot_error(bot, update)
+                    notify_toot_error(update, context)
                 else:
-                    notify_toot_success(bot, update)
+                    notify_toot_success(update, context)
             else:
-                media_id = upload_media(bot, update, mastodon_client, file_path)
+                media_id = upload_media(
+                    update, mastodon_client, file_path)
                 if not mastodon_client.status_post(status="", visibility=visibility, media_ids=media_id).id:
-                    notify_toot_error(bot, update)
+                    notify_toot_error(update, context)
                 else:
-                    notify_toot_success(bot, update)
+                    notify_toot_success(update, context)
             os.remove(file_path)
         else:  # for texts
             toot_text = update.message.text
             if not mastodon_client.status_post(toot_text, visibility=visibility).id:
-                notify_toot_error(bot, update)
+                notify_toot_error(update, context)
             else:
-                notify_toot_success(bot, update)
+                notify_toot_success(update, context)
     else:
-        notify_incomplete_information(bot, update)
+        notify_incomplete_information(update, context)
 
 
 # helper functions
-def verify_account(bot, update, user_id):
+def verify_account(update, context, user_id):
     try:
         mastodon_client = accounts_information[user_id]["mastodon client"]
         mastodon_client.account_verify_credentials()
     except Exception:
         # token or instance not correct
-        bot.send_message(
-            chat_id=update.message.chat_id,
-            text="Your information seems to be incorrect. Please reset your instance url and access token")
+        bot_send_message(
+            update, context, "Your information seems to be incorrect. Please reset your instance url and access token")
     else:
-        bot.send_message(
+        context.bot.send_message(
             chat_id=update.message.chat_id,
             text="Done! Your can now toot as {} to {}".format(
-                mastodon_client.account_verify_credentials()["display_name"],
+                mastodon_client.account_verify_credentials()["display_name"] or mastodon_client.account_verify_credentials()["username"],
                 accounts_information[user_id]["instances address"]), disable_web_page_preview=True)
 
 
@@ -123,27 +130,30 @@ def set_mastodon_client(user_id):
     )
 
 
-def upload_media(bot, update, mastodon_client, file_path):
+def upload_media(update, mastodon_client, file_path):
+
     new_file = update.message.photo[-1].get_file()
     new_file.download(file_path)
     media_id = mastodon_client.media_post(file_path, description=" ").id
     return media_id
 
 
-def notify_incomplete_information(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text='You have not set your mastodon account yet!')
+def notify_incomplete_information(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text='You have not set your mastodon account yet!')
 
 
-def notify_missing_information(bot, update, information):
-    bot.send_message(chat_id=update.message.chat_id, text=information)
+def bot_send_message(update, context, message):
+
+    context.bot.send_message(chat_id=update.message.chat_id, text=message)
 
 
-def notify_toot_success(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text='Tooted!')
+def notify_toot_success(update, context):
+    bot_send_message(update, context, 'Tooted!')
 
 
-def notify_toot_error(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text='Error! Sending toot failed!')
+def notify_toot_error(update, context):
+    bot_send_message(update, context, 'Error! Sending toot failed!')
 
 
 def user_exists(user_id):
@@ -162,7 +172,7 @@ def user_information_is_complete(user_id):
 
 
 # telegram API TOKEN
-updater = Updater(token=TOKEN)
+updater = Updater(TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
 accounts_information = {}
@@ -179,7 +189,8 @@ Format of the instance url should be: https://octodon.social
 You can check out the source code on github
 """
 
-BASE_FILE_PATH = os.path.abspath(os.path.dirname(sys.argv[0])) + '/tmp/{}_{}.png'
+BASE_FILE_PATH = os.path.abspath(
+    os.path.dirname(sys.argv[0])) + '/tmp/{}_{}.png'
 
 # start and help handler
 start_handler = CommandHandler('start', start)
@@ -188,8 +199,8 @@ myinfo_handler = CommandHandler('myinfo', my_info)
 
 # commands handlers
 userid_handler = CommandHandler('id', send_user_id)
-instance_handler = CommandHandler('set_instance', set_instance_address, pass_args=True)
-access_token_handler = CommandHandler('set_accesstoken', set_access_token, pass_args=True)
+instance_handler = CommandHandler('set_instance', set_instance_address)
+access_token_handler = CommandHandler('set_accesstoken', set_access_token)
 
 toot_handler = MessageHandler(filters=Filters.text | Filters.photo, callback=toot)
 
@@ -210,5 +221,9 @@ logging.basicConfig(
 
 # start the bot
 if __name__ == '__main__':
-    create_media_dir()
-    updater.start_polling()
+    try:
+        create_media_dir()
+        updater.start_polling()
+    except KeyboardInterrupt:  # kill script manually
+        print('Interrupted')
+        sys.exit()
